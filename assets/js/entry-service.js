65ccd495fba82b6b03597335c6f60a9236e9b736
Joseph Podwys <joseph.podwys@familysearch.org>
2016-01-10 19:30:25 -0700

    no message

--- assets/js/entry-service.js
+++ assets/js/entry-service.js
@@ -3,35 +3,38 @@ var promise = require('zousan'),
 // require('superagent-promise')(xhr, promise);
 
 exports.getAllEntries = function(){
+  // return new promise(function (resolve, reject){
+  //   xhr.get('/')
+  //     .accept('application/json')
+  //     ._end(function (err, response){
+  //       (err) ? reject(err) : resolve(response.body);
+  //     }
+  //   );
+  // });
   return xhr.get('/')
-    .accept('application/json')
-    // ._end();
+    .accept('application/json');
 }
 
 exports.getEntryById = function(id){
   return xhr.get('/entry/' + id)
-    .accept('application/json')
-    // .end();
+    .accept('application/json');
 }
 
 exports.createEntry = function(entry){
   return xhr.post('/entry')
     .accept('application/json')
     .send(entry)
-    .pruneOptions(['content-type'])
-    // .end();
+    .pruneOptions(['content-type']);
 }
 
 exports.updateEntry = function(entry){
   return xhr.put('/entry/' + entry.id)
     .accept('application/json')
     .send(entry)
-    .pruneOptions(['content-type'])
-    // .end();
+    .pruneOptions(['content-type']);
 }
 
 exports.deleteEntry = function(id){
   return xhr.del('/entry/' + id)
-    .accept('application/json')
-    // .end();
+    .accept('application/json');
 }
