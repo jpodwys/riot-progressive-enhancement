8c998c08c3a46e382e7f80343fbd70515c0b67be
Joseph Podwys <joseph.podwys@familysearch.org>
2016-01-11 01:02:54 -0700

    no message

--- assets/js/entry-service.js
+++ assets/js/entry-service.js
@@ -1,6 +1,5 @@
 var promise = require('zousan'),
-  xhr = require('superagent');
-require('superagent-cache')(xhr, null, {responseProp: 'body'});
+  xhr = require('superagent-cache')(null, null, {responseProp: 'body'});
 require('superagent-promise')(xhr, promise);
 
 exports.getAllEntries = function(){
