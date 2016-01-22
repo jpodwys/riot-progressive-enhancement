51548fed2a82fed55495cd1e4635a80bbe2c3308
Joseph Podwys <joseph.podwys@familysearch.org>
2016-01-21 18:52:42 -0700

    Adding AES encryption to entry text. Executing from the server for security.

--- bl/entry-bl.js
+++ bl/entry-bl.js
@@ -1,4 +1,5 @@
-var promise = require('zousan');
+var promise = require('zousan'),
+  AES = require('../utils/aes');
 
 module.exports = function(Entry){
   var self = this;
@@ -6,6 +7,10 @@ module.exports = function(Entry){
   self.getEntriesByOwnerId = function(data, user){
     return new promise(function (resolve, reject){
       Entry.getEntriesByOwnerId(user.id, 20, 0).then(function (entries){
+        entries.rows = entries.rows.map(function (entry){
+          entry.text = AES.decrypt(entry.text);
+          return entry;
+        });
         return resolve(entries);
       }, function (err){
         return reject({status: 500, message: err});
@@ -24,6 +29,7 @@ module.exports = function(Entry){
         if(!entry.isPublic && (!user || (user.id !== entry.ownerId))){
           return reject({status: 404, message: 'Entry not found.'});
         }
+        entry.text = AES.decrypt(entry.text);
         return resolve(entry);
       }, function (err){
         return reject({status: 500, message: err});
@@ -33,6 +39,7 @@ module.exports = function(Entry){
 
   self.createEntry = function(data, user){
     return new promise(function (resolve, reject){
+      data.text = AES.encrypt(data.text);
       Entry.createEntry(data, user.id).then(function (entry){
         return resolve(entry.id);
       }, function (err){
@@ -46,6 +53,7 @@ module.exports = function(Entry){
       Entry.getEntryById(data.id).then(function (entry){
         if(!entry) return reject({status: 404, message: 'Entry not found.'});
         if(user.id !== entry.ownerId) return reject({status: 404, message: 'Entry not found.'});
+        data.text = AES.encrypt(data.text);
         Entry.updateEntry(data).then(function (response){
           return resolve();
         }, function (err){
