aaa49737d2834e19d20a6e1d608f896034708be7
Joseph Podwys <joseph@podwys.com>
2016-04-20 22:36:50 -0600

    Completing routes.js cleanup.

--- routes.js
+++ routes.js
@@ -1,39 +1,7 @@
 var Sequelize = require('sequelize'),
-
   db = require('./db')(Sequelize),
-
-  // sequelize = new Sequelize(process.env.DB_URL, {
-  //   dialectOptions: {
-  //     debug: true,
-  //     ssl: {
-  //       // Run this command for each SSL ENV
-  //       // heroku config:set SQL_SSL_CA="$(cat ca.pem)" -a riot-demo
-  //       // Run this to pull/push ENVs form heroku to .env or back
-  //       // heroku config:pull -a riot-demo
-  //       // heroku config:push -a riot-demo
-  //       ca: process.env.SQL_SSL_CA,
-  //       cert: process.env.SQL_SSL_CERT,
-  //       key: process.env.SQL_SSL_KEY
-  //     }
-  //   },
-  //   omitNull: true,
-  //   logging: (process.env.NODE_ENV === 'development') ? console.log : false
-  // }),
-
   user = require('./middleware/userMW')(db, Sequelize),
-
-  // userModel = require('./models/user-model')(sequelize, Sequelize),
-  // userService = new (require('./services/user-service'))(userModel, sequelize),
-  // userBl = new (require('./bl/user-bl'))(userService),
-  // user = new (require('./middleware/service-wrapper'))(userBl),
-
   entry = require('./middleware/entryMW')(db, Sequelize),
-
-  // entryModel = require('./models/entry-model')(sequelize, Sequelize),
-  // entryService = new (require('./services/entry-service'))(entryModel, sequelize),
-  // entryBl = new (require('./bl/entry-bl'))(entryService),
-  // entry = new (require('./middleware/service-wrapper'))(entryBl),
-
   resMods = require('./middleware/response-mods'),
   handlers = require('./middleware/handlers');
 
