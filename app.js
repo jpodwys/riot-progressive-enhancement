6b93eaef1a346d5d8d2b16449d8a042dfb0fab99
Joseph Podwys <joseph@podwys.com>
2016-05-10 23:26:31 -0600

    no message

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var express = require('express'),
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.use(compress({threshold: '5kb'}));
+app.use(compress({threshold: '1.4kb'}));
 app.use(forceSsl);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
